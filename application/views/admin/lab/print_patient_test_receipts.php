b10eb8a11d43ce469d5ab73dfb1372d355f792a6
navidaziz98 <navidaziz98@gmail.com>
2024-09-21 15:54:31 +0500

    some changes

--- application/views/admin/lab/print_patient_test_receipts.php
+++ application/views/admin/lab/print_patient_test_receipts.php
@@ -2,61 +2,68 @@
 <html>
 
 <head>
-  <title>Receipt</title>
-  <link rel="stylesheet" type="text/css" href="<?php echo site_url("assets/" . ADMIN_DIR); ?>/css/cloud-admin.css" media="screen,print" />
-  <link rel="stylesheet" type="text/css" href="<?php echo site_url("assets/" . ADMIN_DIR); ?>/css/themes/default.css" media="screen,print" id="skin-switcher" />
+    <title>Receipt</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo site_url("assets/" . ADMIN_DIR); ?>/css/cloud-admin.css"
+        media="screen,print" />
+    <link rel="stylesheet" type="text/css" href="<?php echo site_url("assets/" . ADMIN_DIR); ?>/css/themes/default.css"
+        media="screen,print" id="skin-switcher" />
 
-  <link rel="stylesheet" type="text/css" href="<?php echo site_url("assets/" . ADMIN_DIR); ?>/css/custom.css" media="screen,print" />
+    <link rel="stylesheet" type="text/css" href="<?php echo site_url("assets/" . ADMIN_DIR); ?>/css/custom.css"
+        media="screen,print" />
 
 
-  <style>
+    <style>
     body {
-      background: rgb(204, 204, 204);
+        background: rgb(204, 204, 204);
     }
 
     page {
-      background: white;
-      display: block;
-      margin: 0 auto;
-      margin-bottom: 0.5cm;
-      box-shadow: 0 0 0.5cm rgba(0, 0, 0, 0.5);
+        background: white;
+        display: block;
+        margin: 0 auto;
+        margin-bottom: 0.5cm;
+        box-shadow: 0 0 0.5cm rgba(0, 0, 0, 0.5);
     }
 
     page[size="A4"] {
-      width: 8cm;
-      height: auto;
-      font-weight: bold !important;
-      font-family: "Segoe UI", Frutiger, "Frutiger Linotype", "Dejavu Sans", "Helvetica Neue", Arial, sans-serif;
-      font-size: 12px !important;
+        width: 8cm;
+        height: auto;
+        font-weight: bold !important;
+        font-family: "Segoe UI", Frutiger, "Frutiger Linotype", "Dejavu Sans", "Helvetica Neue", Arial, sans-serif;
+        font-size: 12px !important;
     }
 
 
     @media print {
 
-      body,
-      page {
-        margin: 0;
-        box-shadow: 0;
-        color: black;
-      }
+        body,
+        page {
+            margin: 0;
+            box-shadow: 0;
+            color: black;
+            size: 80mm auto;
+            /* Set paper width to 80mm */
+            margin: 0;
+            /* Remove margins */
+        }
 
-      .table-bordered {
-        border: 1px solid black !important;
-      }
+        .table-bordered {
+            border: 1px solid black !important;
+        }
 
-      .table>thead>tr>th,
-      .table>tbody>tr>th,
-      .table>tfoot>tr>th,
-      .table>thead>tr>td,
-      .table>tbody>tr>td,
-      .table>tfoot>tr>td {
-        padding: 3px;
-        line-height: 1.628571;
-        vertical-align: top;
-        border-top: 1px solid #ddd;
-        color: black !important;
-        border: 1px solid black;
-      }
+        .table>thead>tr>th,
+        .table>tbody>tr>th,
+        .table>tfoot>tr>th,
+        .table>thead>tr>td,
+        .table>tbody>tr>td,
+        .table>tfoot>tr>td {
+            padding: 3px;
+            line-height: 1.628571;
+            vertical-align: top;
+            border-top: 1px solid #ddd;
+            color: black !important;
+            border: 1px solid black;
+        }
 
     }
 
@@ -67,46 +74,48 @@
     .table>thead>tr>td,
     .table>tbody>tr>td,
     .table>tfoot>tr>td {
-      padding: 3px;
-      line-height: 1.628571;
-      vertical-align: top;
-      border-top: 1px solid #ddd;
-      color: black !important;
-      border: 1px solid black;
+        padding: 3px;
+        line-height: 1.628571;
+        vertical-align: top;
+        border-top: 1px solid #ddd;
+        color: black !important;
+        border: 1px solid black;
     }
-  </style>
+    </style>
 </head>
 
 <body>
-  <page size='A4'>
+    <page size='A4'>
 
 
 
-    <table style="width: 99%; margin: 2px; padding:2px; ">
-      <thead>
-        <tr>
-          <td>
-            <table style="width: 100%; margin-top: 10px; color:black">
+        <table style="width: 99%; margin: 2px; padding:2px; ">
+            <thead>
+                <tr>
+                    <td>
+                        <table style="width: 100%; margin-top: 10px; color:black">
 
-              <tr>
-                <td style="text-align: center;">
-                  <h4><?php echo $system_global_settings[0]->system_title ?> Chitral</h4>
-                  <h6 style="font-size: 11px;"><?php echo $system_global_settings[0]->address; ?>
-                    <br /> PHONE <?php echo $system_global_settings[0]-> phone_number; ?>
-                  </h6>
-                  <h5>RECEIPT NO: <?php echo $invoice_detail->invoice_id; ?>
-                    <?php if ($invoice_detail->receipt_print == 0) { ?>
-                      <span style="font-size: 17px; display: block; margin-top: 5px;">Token NO: <?php echo $invoice_detail->test_token_id; ?></span>
-                    <?php
+                            <tr>
+                                <td style="text-align: center;">
+                                    <h4><?php echo $system_global_settings[0]->system_title ?> Chitral</h4>
+                                    <h6 style="font-size: 11px;"><?php echo $system_global_settings[0]->address; ?>
+                                        <br /> PHONE <?php echo $system_global_settings[0]-> phone_number; ?>
+                                    </h6>
+                                    <h5>RECEIPT NO: <?php echo $invoice_detail->invoice_id; ?>
+                                        <?php if ($invoice_detail->receipt_print == 0) { ?>
+                                        <span style="font-size: 17px; display: block; margin-top: 5px;">Token NO:
+                                            <?php echo $invoice_detail->test_token_id; ?></span>
+                                        <?php
                       $query = "UPDATE `invoices` SET `invoices`.`receipt_print`=1 WHERE `invoices`.`invoice_id` = '" . $invoice_detail->invoice_id . "'";
                       $this->db->query($query);
                     } else { ?>
-                      <span style="font-size: 17px; display: block; margin-top: 5px;">Token NO: ******<?php echo substr($invoice_detail->test_token_id, -4); ?></span>
+                                        <span style="font-size: 17px; display: block; margin-top: 5px;">Token NO:
+                                            ******<?php echo substr($invoice_detail->test_token_id, -4); ?></span>
 
-                    <?php } ?>
-                    <h4>
-                      Appointment No:
-                      <?php
+                                        <?php } ?>
+                                        <h4>
+                                            Appointment No:
+                                            <?php
                       if ($invoice_detail->category_id != 5) {
 
                         $query = "SELECT test_category FROM test_categories WHERE test_category_id= '" . $invoice_detail->category_id . "'";
@@ -119,100 +128,111 @@
                         echo "<br />" . $opd_doctor . '<br />';
                         echo date("d F, Y ", strtotime($invoice_detail->created_date));
                       } ?>
-                    </h4>
-                  </h5>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <h5 style="border: 1px dashed  black; padding: 2px; color:black">
-                    <table width="100%" style="font-size: 15px;">
-                      <tr>
-                        <td width="100">Patient: </td>
-                        <td style="font-size: 20px;"><?php echo trim(ucwords(strtolower($invoice_detail->patient_name))); ?></td>
-                      </tr>
-                      <!--                       
+                                        </h4>
+                                    </h5>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <h5 style="border: 1px dashed  black; padding: 2px; color:black">
+                                        <table width="100%" style="font-size: 15px;">
+                                            <tr>
+                                                <td width="100">Patient: </td>
+                                                <td style="font-size: 20px;">
+                                                    <?php echo trim(ucwords(strtolower($invoice_detail->patient_name))); ?>
+                                                </td>
+                                            </tr>
+                                            <!--                       
                       <tr>
                         <td>Mobile No:</td>
                         <td><?php echo $invoice_detail->patient_mobile_no; ?></td>
                       </tr> -->
-                      <tr>
-                        <td colspan="2">Gender: <span style="font-size: 20px;"><?php echo $invoice_detail->patient_gender; ?></span>
-                          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Age: <span style="font-size: 20px;"><?php echo @$invoice_detail->patient_age; ?> Y </span></td>
-                      </tr>
+                                            <tr>
+                                                <td colspan="2">Gender: <span
+                                                        style="font-size: 20px;"><?php echo $invoice_detail->patient_gender; ?></span>
+                                                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Age:
+                                                    <span
+                                                        style="font-size: 20px;"><?php echo @$invoice_detail->patient_age; ?>
+                                                        Y </span>
+                                                </td>
+                                            </tr>
 
-                      <tr>
-                        <td>Address</td>
-                        <td><?php echo trim(ucwords(strtolower($invoice_detail->patient_address))); ?></td>
-                      </tr>
+                                            <tr>
+                                                <td>Address</td>
+                                                <td><?php echo trim(ucwords(strtolower($invoice_detail->patient_address))); ?>
+                                                </td>
+                                            </tr>
 
-                      <?php if ($invoice_detail->category_id != 5) { ?>
-                        <tr>
-                          <td>Refereed By:</td>
-                          <td><?php echo str_replace("Muhammad", "M.", $invoice_detail->doctor_name) . "( " . $invoice_detail->doctor_designation . " )"; ?></td>
-                        </tr>
-                      <?php } ?>
-                      <tr>
-                        <td>Date & Time:</td>
-                        <td><?php echo date("d F, Y h:i:s", strtotime($invoice_detail->created_date)); ?></td>
-                      </tr>
-                    </table>
-                    <h5>
-                </td>
+                                            <?php if ($invoice_detail->category_id != 5) { ?>
+                                            <tr>
+                                                <td>Refereed By:</td>
+                                                <td><?php echo str_replace("Muhammad", "M.", $invoice_detail->doctor_name) . "( " . $invoice_detail->doctor_designation . " )"; ?>
+                                                </td>
+                                            </tr>
+                                            <?php } ?>
+                                            <tr>
+                                                <td>Date & Time:</td>
+                                                <td><?php echo date("d F, Y h:i:s", strtotime($invoice_detail->created_date)); ?>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                        <h5>
+                                </td>
 
-              </tr>
+                            </tr>
 
-              <tr>
-                <td>
+                            <tr>
+                                <td>
 
-                  <h5>
-                    <table border="1" width="100%" style="border-collapse:collapse; color:black; font-size: 15px;">
-                      <tr>
-                        <td>#</td>
-                        <td>Details</td>
-                        <td>Amount</td>
-                      </tr>
-                      <?php
+                                    <h5>
+                                        <table border="1" width="100%"
+                                            style="border-collapse:collapse; color:black; font-size: 15px;">
+                                            <tr>
+                                                <td>#</td>
+                                                <td>Details</td>
+                                                <td>Amount</td>
+                                            </tr>
+                                            <?php
                       $count = 1;
                       foreach ($invoice->invoice_details as $invoicedetail) { ?>
-                        <tr>
-                          <td><?php echo $count++; ?></td>
-                          <td>
-                            <?php if ($invoice_detail->category_id != 5) { ?>
-                              <?php echo $invoicedetail->test_group_name; ?>
-                            <?php } else {
+                                            <tr>
+                                                <td><?php echo $count++; ?></td>
+                                                <td>
+                                                    <?php if ($invoice_detail->category_id != 5) { ?>
+                                                    <?php echo $invoicedetail->test_group_name; ?>
+                                                    <?php } else {
                               echo "Consultation Fee";
                             } ?>
-                          </td>
-                          <td><?php echo $invoicedetail->price; ?></td>
-                        </tr>
-                      <?php } ?>
-                      <tr>
-                        <th colspan="3" style="text-align: right;">
-                          <h4>
-                            Total: <?php echo $invoice->price; ?>.00 Rs <br />
-                            Discount: <?php if ($invoice->discount) {
+                                                </td>
+                                                <td><?php echo $invoicedetail->price; ?></td>
+                                            </tr>
+                                            <?php } ?>
+                                            <tr>
+                                                <th colspan="3" style="text-align: right;">
+                                                    <h4>
+                                                        Total: <?php echo $invoice->price; ?>.00 Rs <br />
+                                                        Discount: <?php if ($invoice->discount) {
                                         echo $invoice->discount;
                                       } else {
                                         echo "00";
                                       }  ?>.00 Rs <br />
-                            Total: <?php echo $invoice->total_price; ?>.00 Rs
-                          </h4>
-                </td>
-              </tr>
+                                                        Total: <?php echo $invoice->total_price; ?>.00 Rs
+                                                    </h4>
+                                </td>
+                            </tr>
 
-            </table>
-            </h5>
-          </td>
+                        </table>
+                        </h5>
+                    </td>
+                </tr>
+        </table>
+        </td>
         </tr>
-    </table>
-    </td>
-    </tr>
-    </thead>
+        </thead>
 
-    </table>
-    <p style="font-size: smaller; font-weight: initial; text-align: center; color:black">Data entered by:
-      <?php $query = "SELECT user_title from users WHERE user_id='" . $invoice->created_by . "'";
+        </table>
+        <p style="font-size: smaller; font-weight: initial; text-align: center; color:black">Data entered by:
+            <?php $query = "SELECT user_title from users WHERE user_id='" . $invoice->created_by . "'";
       echo $this->db->query($query)->result()[0]->user_title;
       $query = "SELECT
                   `roles`.`role_title` 
@@ -222,9 +242,9 @@
               AND `users`.`user_id`='" . $invoice->created_by . "'";
       echo " (" . $this->db->query($query)->result()[0]->role_title . ")";
       ?> </p>
-    </div>
+        </div>
 
-  </page>
+    </page>
 </body>
 
 
