5ca4f054c171720f3a447a410185fc693c010919
Navid Aziz <navidaziz98@gmail.com>
2025-05-20 16:27:12 +0500

    some changes

--- application/views/admin/lab/print_test_report copy 2.php
+++ application/views/admin/lab/print_test_report copy 2.php
@@ -26,217 +26,217 @@
 
 
     <style>
-        body {
-            background: rgb(204, 204, 204);
+    body {
+        background: rgb(204, 204, 204);
 
-            font-family: "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+        font-family: "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
 
-        }
+    }
 
 
 
-        element.style {
+    element.style {
+        color: black;
+        font-weight: bold;
+    }
+
+
+    page {
+        background: white;
+        display: block;
+        margin: 0 auto;
+        margin-bottom: 0.5cm;
+        box-shadow: 0 0 0.5cm rgba(0, 0, 0, 0.5);
+
+    }
+
+
+
+    page[size="A4"] {
+        width: 21cm;
+        /* height: 29.7cm; */
+        height: auto;
+    }
+
+    page[size="A4"][layout="landscape"] {
+        width: 29.7cm;
+        height: 21cm;
+    }
+
+    page[size="A3"] {
+        width: 29.7cm;
+        height: 42cm;
+    }
+
+    page[size="A3"][layout="landscape"] {
+        width: 42cm;
+        height: 29.7cm;
+    }
+
+    page[size="A5"] {
+        width: 14.8cm;
+        height: 21cm;
+    }
+
+    page[size="A5"][layout="landscape"] {
+        width: 21cm;
+        height: 14.8cm;
+    }
+
+    @media print {
+
+        body,
+        page {
+            margin: 0;
+            box-shadow: 0;
             color: black;
-            font-weight: bold;
+
         }
 
+
+
+    }
+
+
+    .table>thead>tr>th,
+    .table>tbody>tr>th,
+    .table>tfoot>tr>th,
+    .table>thead>tr>td,
+    .table>tbody>tr>td,
+    .table>tfoot>tr>td {
+        padding: 4px;
+        line-height: 1;
+        vertical-align: top;
+        border-top: 1px solid #ddd;
+        font-size: 12px !important;
+        color: black;
+    }
+
+    /* Styles go here */
+    @media screen {
+        .print-page-header {
+            height: auto;
+            display: none;
+        }
+    }
+
+
+
+
+    @media screen {
+        .page-footer {
+            height: 50px;
+            display: none;
+        }
+    }
+
+
+
+    @media print {
+        .page-footer {
+            position: fixed;
+            bottom: 0;
+            width: 100%;
+            border-top: 1px solid gray;
+            /* for demo */
+            content: counter(page) " of "counter(pages);
+            /* for demo */
+        }
+
+        .page-footer-space {
+            height: 80px;
+
+        }
+    }
+
+    @media screen {
+        .page-footer {
+            position: relative;
+
+            width: 100%;
+            border-top: 1px solid gray;
+            /* for demo */
+            display: block;
+            /* for demo */
+        }
+
+        .page-footer-space {
+            height: 80px;
+            display: none;
+        }
+    }
+
+    @media print {
+        .print-page-header {
+            position: fixed;
+            top: 0mm;
+            width: 100%;
+            background: yellow;
+            /* for demo */
+            /* for demo */
+        }
+
+        .print-page-header-space {
+            height: 90px;
+        }
+    }
+
+    @media screen {
+        .print-page-header {
+            position: relative;
+            top: 0mm;
+            width: 100%;
+            display: block;
+            /* for demo */
+            /* for demo */
+        }
+
+        .print-page-header-space {
+            height: 0px;
+            display: none;
+        }
+    }
+
+
+
+
+    .page {
+        page-break-after: always;
+    }
+
+
+
+    @page {
+        margin: 20mm
+    }
+
+    @media print {
+        thead {
+            display: table-header-group;
+        }
+
+        tfoot {
+            display: table-footer-group;
+        }
+
+        button {
+            display: none;
+        }
+
+        body {
+            margin: 0;
+            background-color: white !important;
+        }
 
         page {
             background: white;
             display: block;
             margin: 0 auto;
             margin-bottom: 0.5cm;
-            box-shadow: 0 0 0.5cm rgba(0, 0, 0, 0.5);
+            box-shadow: none !important;
 
         }
-
-
-
-        page[size="A4"] {
-            width: 21cm;
-            /* height: 29.7cm; */
-            height: auto;
-        }
-
-        page[size="A4"][layout="landscape"] {
-            width: 29.7cm;
-            height: 21cm;
-        }
-
-        page[size="A3"] {
-            width: 29.7cm;
-            height: 42cm;
-        }
-
-        page[size="A3"][layout="landscape"] {
-            width: 42cm;
-            height: 29.7cm;
-        }
-
-        page[size="A5"] {
-            width: 14.8cm;
-            height: 21cm;
-        }
-
-        page[size="A5"][layout="landscape"] {
-            width: 21cm;
-            height: 14.8cm;
-        }
-
-        @media print {
-
-            body,
-            page {
-                margin: 0;
-                box-shadow: 0;
-                color: black;
-
-            }
-
-
-
-        }
-
-
-        .table>thead>tr>th,
-        .table>tbody>tr>th,
-        .table>tfoot>tr>th,
-        .table>thead>tr>td,
-        .table>tbody>tr>td,
-        .table>tfoot>tr>td {
-            padding: 4px;
-            line-height: 1;
-            vertical-align: top;
-            border-top: 1px solid #ddd;
-            font-size: 12px !important;
-            color: black;
-        }
-
-        /* Styles go here */
-        @media screen {
-            .print-page-header {
-                height: auto;
-                display: none;
-            }
-        }
-
-
-
-
-        @media screen {
-            .page-footer {
-                height: 50px;
-                display: none;
-            }
-        }
-
-
-
-        @media print {
-            .page-footer {
-                position: fixed;
-                bottom: 0;
-                width: 100%;
-                border-top: 1px solid gray;
-                /* for demo */
-                content: counter(page) " of " counter(pages);
-                /* for demo */
-            }
-
-            .page-footer-space {
-                height: 80px;
-
-            }
-        }
-
-        @media screen {
-            .page-footer {
-                position: relative;
-
-                width: 100%;
-                border-top: 1px solid gray;
-                /* for demo */
-                display: block;
-                /* for demo */
-            }
-
-            .page-footer-space {
-                height: 80px;
-                display: none;
-            }
-        }
-
-        @media print {
-            .print-page-header {
-                position: fixed;
-                top: 0mm;
-                width: 100%;
-                background: yellow;
-                /* for demo */
-                /* for demo */
-            }
-
-            .print-page-header-space {
-                height: 90px;
-            }
-        }
-
-        @media screen {
-            .print-page-header {
-                position: relative;
-                top: 0mm;
-                width: 100%;
-                display: block;
-                /* for demo */
-                /* for demo */
-            }
-
-            .print-page-header-space {
-                height: 0px;
-                display: none;
-            }
-        }
-
-
-
-
-        .page {
-            page-break-after: always;
-        }
-
-
-
-        @page {
-            margin: 20mm
-        }
-
-        @media print {
-            thead {
-                display: table-header-group;
-            }
-
-            tfoot {
-                display: table-footer-group;
-            }
-
-            button {
-                display: none;
-            }
-
-            body {
-                margin: 0;
-                background-color: white !important;
-            }
-
-            page {
-                background: white;
-                display: block;
-                margin: 0 auto;
-                margin-bottom: 0.5cm;
-                box-shadow: none !important;
-
-            }
-        }
+    }
     </style>
 </head>
 
@@ -244,7 +244,7 @@
     <page size='A4'>
 
         <div class="print-page-header" style="background-color: rgb(229, 228, 226) !important;">
-            <!-- <table style="width:100%">
+            <table style="width:100%">
                 <tr>
                     <td style="padding-top: 10px; width: 90px !important;">
                         <img src="<?php echo site_url("assets/uploads/" . $system_global_settings[0]->sytem_admin_logo); ?>"
@@ -280,7 +280,7 @@
                     </td>
                 </tr>
 
-            </table> -->
+            </table>
         </div>
 
 
@@ -299,8 +299,7 @@
                                                 style="text-align: left; width:100%; font-size: 12px !important; color:black;">
                                                 <tr>
                                                     <th>Patient ID: <?php echo $invoice_detail->patient_id; ?></th>
-                                                    <!-- <td>History No: <?php //echo $invoice_detail->history_file_no; 
-                                                                            ?>
+                                                    <!-- <td>History No: <?php //echo $invoice_detail->history_file_no; ?>
                                                     </td> -->
                                                 </tr>
                                                 <tr>
@@ -349,8 +348,7 @@
                                                 </tr>
                                                 <!-- <tr>
                                                     <th>Received:</th>
-                                                    <td><?php //echo date("d M, Y h:i:s", strtotime($invoice_detail->process_date)); 
-                                                        ?>
+                                                    <td><?php //echo date("d M, Y h:i:s", strtotime($invoice_detail->process_date)); ?>
                                                     </td>
                                                 </tr> -->
                                                 <!-- <tr>
@@ -372,85 +370,84 @@
                     <tr>
                         <td>
                             <?php
-                            $count = 1;
-                            foreach ($patient_tests_groups as $patient_tests_group) { ?>
-                                <h5 style="margin:5px;  font-weight: bold">
-                                    <?php echo $patient_tests_group->test_group_name; ?>
-                                </h5>
+              $count = 1;
+              foreach ($patient_tests_groups as $patient_tests_group) { ?>
+                            <h5 style="margin:5px;  font-weight: bold">
+                                <?php echo $patient_tests_group->test_group_name; ?>
+                            </h5>
+                            <?php
+
+                $normal_value = false;
+                foreach ($patient_tests_group->patient_tests as $patient_test) {
+                  //if ($patient_test->test_result != '') {
+                  if (trim($patient_test->test_normal_value) != "") {
+                    $normal_value = true;
+                  }
+                  //}
+                }
+                ?>
+
+                            <table class="table table-bordered" style="text-align: left; font-size:10px; width:100%">
+                                <tr>
+                                    <!-- <th >#</th> -->
+                                    <th style="width: 200px;">TEST(s)</th>
+                                    <th style="width: 200px;">RESULT(s)</th>
+                                    <?php if ($normal_value) { ?>
+                                    <th style="width: 100px;">UNIT(s)</th>
+                                    <th style="width: 300px;">NORMALS</th>
+                                    <?php } ?>
+                                </tr>
+
+
+
                                 <?php
 
-                                $normal_value = false;
-                                foreach ($patient_tests_group->patient_tests as $patient_test) {
-                                    //if ($patient_test->test_result != '') {
-                                    if (trim($patient_test->test_normal_value) != "") {
-                                        $normal_value = true;
-                                    }
-                                    //}
-                                }
-                                ?>
+                  foreach ($patient_tests_group->patient_tests as $patient_test) { ?>
+                                <?php //if ($patient_test->test_result != '') { 
+                    ?>
+                                <?php if ($count == 1) { ?>
 
-                                <table class="table table-bordered" style="text-align: left; font-size:10px; width:100%">
-                                    <tr>
-                                        <!-- <th >#</th> -->
-                                        <th style="width: 200px;">TEST(s)</th>
-                                        <th style="width: 200px;">RESULT(s)</th>
-                                        <?php if ($normal_value) { ?>
-                                            <th style="width: 100px;">UNIT(s)</th>
-                                            <th style="width: 300px;">NORMALS</th>
-                                        <?php } ?>
-                                    </tr>
+                                <?php } ?>
+                                <tr>
+                                    <th><?php echo $patient_test->test_name; ?></th>
+                                    <th> <?php echo $patient_test->test_result; ?>
+                                        <?php //echo $patient_test->result_suffix; ?></th>
+                                    <?php if ($normal_value) { ?>
+                                    <th style="text-align: center;"> <small> <?php echo $patient_test->unit; ?> </small>
+                                    </th>
 
-
-
-                                    <?php
-
-                                    foreach ($patient_tests_group->patient_tests as $patient_test) { ?>
-                                        <?php //if ($patient_test->test_result != '') { 
-                                        ?>
-                                        <?php if ($count == 1) { ?>
-
-                                        <?php } ?>
-                                        <tr>
-                                            <th><?php echo $patient_test->test_name; ?></th>
-                                            <th> <?php echo $patient_test->test_result; ?>
-                                                <?php //echo $patient_test->result_suffix; 
-                                                ?></th>
-                                            <?php if ($normal_value) { ?>
-                                                <th style="text-align: center;"> <small> <?php echo $patient_test->unit; ?> </small>
-                                                </th>
-
-                                                <th style="width: 300px;">
-                                                    <small><?php echo $patient_test->test_normal_value; ?></small>
-                                                </th>
-                                            <?php } ?>
-                                        </tr>
-                                        <?php //} 
-                                        ?>
+                                    <th style="width: 300px;">
+                                        <small><?php echo $patient_test->test_normal_value; ?></small>
+                                    </th>
                                     <?php } ?>
-                                </table>
+                                </tr>
+                                <?php //} 
+                    ?>
+                                <?php } ?>
+                            </table>
                             <?php  } ?>
                             <br />
                             <?php if ($invoice_detail->remarks) { ?>
-                                <div style="text-align: left; color:black"><strong>Remarks:</strong>
-                                    <br />
-                                    <?php echo $invoice_detail->remarks; ?>
-                                </div>
+                            <div style="text-align: left; color:black"><strong>Remarks:</strong>
+                                <br />
+                                <?php echo $invoice_detail->remarks; ?>
+                            </div>
                             <?php } ?>
 
                             <br />
                             <?php
-                            $query = "SELECT `test_report_by` FROM `invoices` WHERE `invoice_id`= '" . $invoice_detail->invoice_id . "' ";
-                            $lab_technician_id = $this->db->query($query)->result()[0]->test_report_by;
+              $query = "SELECT `test_report_by` FROM `invoices` WHERE `invoice_id`= '" . $invoice_detail->invoice_id . "' ";
+              $lab_technician_id = $this->db->query($query)->result()[0]->test_report_by;
 
-                            $query = "SELECT
+              $query = "SELECT
                   `roles`.`role_title`,
                   `users`.`user_title`  
               FROM `roles`,
               `users` 
               WHERE `roles`.`role_id` = `users`.`role_id`
               AND `users`.`user_id`='" . $lab_technician_id . "'";
-                            $user_data = $this->db->query($query)->result()[0];
-                            ?>
+              $user_data = $this->db->query($query)->result()[0];
+              ?>
                             <div style="text-align: right;">
                                 <small style="text-align: right;">
                                     <b><?php echo $user_data->user_title; ?> (<?php echo $user_data->role_title; ?>)</b>
@@ -482,15 +479,15 @@
                 Print @ <?php echo date("d M, Y h:m:s A"); ?>
                 by
                 <?php
-                $query = "SELECT
+        $query = "SELECT
                 `roles`.`role_title`,
                 `users`.`user_title`  
             FROM `roles`,
             `users` 
             WHERE `roles`.`role_id` = `users`.`role_id`
             AND `users`.`user_id`='" . $this->session->userdata("user_id") . "'";
-                $user_data = $this->db->query($query)->result()[0];
-                ?>
+        $user_data = $this->db->query($query)->result()[0];
+        ?>
                 <?php echo $user_data->user_title; ?> (<?php echo $user_data->role_title; ?>)
             </small>
 
